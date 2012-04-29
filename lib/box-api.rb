b39deaa4b644195dd336b8c686fa3990e4a3a58e
Alex Levy <alex.levy@alumni.tufts.edu>
2012-04-29 17:31:16 -0400

    include box/group and box/user

--- lib/box-api.rb
+++ lib/box-api.rb
@@ -10,7 +10,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# These three are the only classes that should be accessed directly
+# These are the only classes that should be accessed directly
 require 'box/account'
 require 'box/file'
 require 'box/folder'
+require 'box/group'
+require 'box/user'
